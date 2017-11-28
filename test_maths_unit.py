6eec88ec3fe86284b69760651ff9294f2a97f294
Nuli Dharma Teja <dt.cb3559@gmail.com>
2017-11-29 03:02:03 +0530

    pytest

--- test_maths_unit.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import maths
-
-class TestMaths(unittest.TestCase):
-	def test_sum(self):
-		self.assertEqual(maths.sum(10, 5), 15)
-		
-if __name__ == "__main__":
-	unittest.main()
