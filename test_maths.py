bfcf3a394255ff569a8c8de4eccab5c3846729c6
Nuli Dharma Teja <dt.cb3559@gmail.com>
2017-11-29 03:17:38 +0530

    corrected indentations again

--- test_maths.py
+++ test_maths.py
@@ -6,7 +6,7 @@ import pytest
 class TestMath(object):
 	def test_sum(self):
 		assert maths.sum(3, 4) == 8
-    	
-    def test_sum_type(self):
-    	with pytest.raises(TypeError):
+		
+	def test_sum_type(self):
+		with pytest.raises(TypeError):
 			maths.sum("dharma", 10)
