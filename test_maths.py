f29f184a7887bd96a66ed3399c7e141694a4be8b
Nuli Dharma Teja <dt.cb3559@gmail.com>
2017-11-29 03:23:47 +0530

    making test pass this time

--- test_maths.py
+++ test_maths.py
@@ -5,7 +5,7 @@ import pytest
 
 class TestMath(object):
     def test_sum(self):
-        assert maths.sum(3, 4) == 8
+        assert maths.sum(3, 4) == 7
         
     def test_sum_type(self):
         with pytest.raises(TypeError):
