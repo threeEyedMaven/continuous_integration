8501f46c7436b16a02c22613a55d6f817298a5d3
Nuli Dharma Teja <dt.cb3559@gmail.com>
2017-11-29 03:22:05 +0530

    replaced tabs with spaces

--- test_maths.py
+++ test_maths.py
@@ -4,9 +4,9 @@ import maths
 import pytest
 
 class TestMath(object):
-	def test_sum(self):
-		assert maths.sum(3, 4) == 8
-		
-	def test_sum_type(self):
-		with pytest.raises(TypeError):
-			maths.sum("dharma", 10)
+    def test_sum(self):
+        assert maths.sum(3, 4) == 8
+        
+    def test_sum_type(self):
+        with pytest.raises(TypeError):
+            maths.sum("dharma", 10)
