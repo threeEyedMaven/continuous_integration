52f2863f27e4038d8d0bf8330575c80cb4407c46
Nuli Dharma Teja <dt.cb3559@gmail.com>
2017-11-29 03:12:06 +0530

    corrected indentations and shifted installations to requirements.txt

--- test_maths.py
+++ test_maths.py
@@ -4,8 +4,8 @@ import maths
 import pytest
 
 class TestMath(object):
-    def test_sum(self):
-	assert maths.sum(3, 4) == 8
+	def test_sum(self):
+		assert maths.sum(3, 4) == 8
     	
     def test_sum_type(self):
     	with pytest.raises(TypeError):
