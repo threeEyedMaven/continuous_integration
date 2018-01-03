a1fd160d1a1ef8e986ac64aa5aaccde145b94579
Nuli Dharma Teja <dt.cb3559@gmail.com>
2018-01-03 17:54:55 +0530

    failing test

--- test/test_maths.py
+++ test/test_maths.py
@@ -14,7 +14,7 @@ class TestMath(object):
             maths.fibonacci("dharma")
 
     def test_sum(self):
-        assert maths.sum(3, 4) == 7
+        assert maths.sum(3, 4) == 8
         
     def test_sum_type(self):
         with pytest.raises(TypeError):
